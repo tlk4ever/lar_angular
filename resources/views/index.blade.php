6ea372914c36dfc2c2b48d1239ac90673235d29e
Thiago Brito <tbffreitas@gmail.com>
2019-07-16 22:08:01 -0300

    run build em public

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <app-root></app-root>
+        <script type="text/javascript" src="{{asset('js/runtime.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/polyfills.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/styles.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/vendor.js')}}"></script>
+        <script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+    </body>
+</html>
